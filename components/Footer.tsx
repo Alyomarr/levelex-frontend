f1ca9deaebda82c3b6fdf57522ed37809515109b
Alyomarr <alyomar105@gmail.com>
2025-11-30 18:43:02 +0200

    Replace blob morphing with gooey effect - icons flow into each other like liquid

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -70,6 +70,26 @@ export default function Footer() {
           <p>© 2025 Levelex. All rights reserved.</p>
         </div>
       </div>
+
+      {/* SVG Filter for Gooey Effect */}
+      <svg style={{ position: "absolute", width: 0, height: 0 }}>
+        <defs>
+          <filter id="gooey">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
+              result="gooey"
+            />
+            <feComposite in="SourceGraphic" in2="gooey" operator="atop" />
+          </filter>
+        </defs>
+      </svg>
     </footer>
   );
 }
